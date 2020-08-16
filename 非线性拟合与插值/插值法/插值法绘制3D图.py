ee331bbdb77275dc92ec194aac93fc5bff9180ec
Lee SunBowen <1554849150@qq.com>
2020-08-16 14:31:26 +0800

    完善

--- 非线性拟合与插值/插值法/插值法绘制3D图.py
+++ 非线性拟合与插值/插值法/插值法绘制3D图.py
@@ -1,29 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 11 12:25:24 2020
+Spyder Editor
 
-@author: LiSunBowen
+This is a temporary script file.
 """
 
+
+from scipy.interpolate import griddata
 import numpy as np
 import matplotlib.pyplot as plt
-from math import exp
-from numpy.linalg import *
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.interpolate import griddata
-x=[129.0,140.0,103.5,88.0,185.5,195.0,105.5,57.5,107.5,77.0,81.0,162.0,162.0,117.5]
-y=[7.5,141.5,23.0,147.0,22.51,37.5,85.5,-6.5,-81,3.0,56.5,-66.5,84.0,-33.5]
-z=np.array([4,8,6,8,6,8,8,9,9,8,8,9,4,9])
-x1 = np.linspace(75,200)
-y1 = np.linspace(-50,150)
-X,Y=np.meshgrid(x1,y1)
-point = []
-for i in range(len(x)):
-    newp = [x[i],y[i]]
-    point.append(newp)
-point = np.array(point)
-znew = griddata(point,z,(X,Y),method='cubic') # 插值
-fig=plt.figure() #画图
-ax=Axes3D(fig)
-ax.plot_surface(X,Y,znew)
+points = np.array([[129,7.5],[140,141.5],[103.5,23],[88,147],[185.5,25.5],[195,137.5],[105,85.5],[157,-6.5],[107.5,81],[77,3],[81,56.5],[162,-66.5],[162,84],[117.5,-33.5]])
+values = np.array([4,8,6,8,6,8,8,9,9,8,8,9,4,9]) #已知散点的值
+xo = np.linspace(75,200,100)
+yo = np.linspace(-50,150,100)
+XO,YO = np.meshgrid(xo,yo)
+ZO =griddata(points,values,(XO,YO),method='cubic')
+xi, yi = np.mgrid[75:200:100j, -50:150:100j] # meshgride参数为数组，mgrid参数类似range()可以处理多维数据
+znew = griddata(points,values,(xi,yi),method='cubic') #进行插值 
+fig=plt.figure()#画图
+ax = Axes3D(fig)
+ax.plot_surface(xi,yi,znew,rstride=2,cstride=2,edgecolor='grey') # 插值法作图似乎无法上色
 plt.show()
